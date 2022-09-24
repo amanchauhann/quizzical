b3f3afd1551318c1cc73fe71279f5514bdc00e69
Aman Chauhan <amangotpro@gmail.com>
2022-09-24 21:11:35 +0530

    add: random option

---
+++ src/Quiz.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Question from './Question.js'
+
+
+function Quiz() {
+
+    
+  return (
+    <div>Hello from quiz
+        <Question
+            correct_answer="aman"
+        />
+    </div>
+  )
+}
+
+export default Quiz
