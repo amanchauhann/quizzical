ed92a17e7e4bbc6a4747bde5d595a0409301357a
zeel-codder <zeelprajapati321@gmail.com>
2022-09-25 21:54:48 +0530

    fix color thing

--- src/Question.js
+++ src/Question.js
@@ -1,48 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function Question(props) {
-    const [isHeld, setIsHeld] = useState(false)
-    const [options, setOptions] = useState([])
+	const [isHeld, setIsHeld] = useState(false);
+	const [options, setOptions] = useState([]);
 
-    function selectOption() {
-        setIsHeld((prevIsHeld) => !prevIsHeld)
-        console.log(isHeld)
-    }
+	function selectOption() {
+		setIsHeld((prevIsHeld) => !prevIsHeld);
+		console.log(isHeld);
+	}
+	console.log(isHeld);
 
-    const styles = {
-        backgroundColor: isHeld ? "gray" : "none"
-    }
+	const styles = {
+		backgroundColor: isHeld ? "gray" : "none",
+	};
 
-    // this is array for putting all the options
-    let randomOption = [];
-    
-    for(let i=0; i<3; i++){
-        randomOption.push(<button className='answer' onClick={props.selectOption} style={styles}>{props.incorrect_answers[i]}</button>)
-    }
-    randomOption.push(<button className='answer' onClick={props.selectOption} style={styles} >{props.correct_answer}</button>)
+	// this is array for putting all the options
 
-    useEffect(()=>{
-        let i = randomOption.length, j, temp
-            while(--i > 0){
-        j= Math.floor(Math.random() * (i+1))
-        temp = randomOption[j]
-        randomOption[j] = randomOption[i]
-        randomOption[i] = temp
-    }
-    setOptions(randomOption)
-    }, [])
-    // Fisher-Yates Shuffle Modern Algorithm for random options for answers.
-    
+	useEffect(() => {
+		let randomOption = [];
 
+		for (let i = 0; i < 3; i++) {
+			randomOption.push(
+				<button className="answer" onClick={selectOption} style={styles}>
+					{props.incorrect_answers[i]}
+				</button>
+			);
+		}
+		randomOption.push(
+			<button className="answer" onClick={selectOption} style={styles}>
+				{props.correct_answer}
+			</button>
+		);
+		let i = randomOption.length,
+			j,
+			temp;
+		while (--i > 0) {
+			j = Math.floor(Math.random() * (i + 1));
+			temp = randomOption[j];
+			randomOption[j] = randomOption[i];
+			randomOption[i] = temp;
+		}
+		setOptions(randomOption);
+	}, [isHeld]);
+	// Fisher-Yates Shuffle Modern Algorithm for random options for answers.
 
-    return (
-        <div>
-            <h3 className="question">{props.question}</h3>
-            <div className='answerContainer'>
-                {options}
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<h3 className="question">{props.question}</h3>
+			<div className="answerContainer">{options}</div>
+		</div>
+	);
 }
 
-export default Question
+export default Question;
